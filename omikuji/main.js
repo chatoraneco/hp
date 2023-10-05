fd6a805a2c06785cbc53f91da5a8b4f45455c9c2
chatoraneco <131725434+chatoraneco@users.noreply.github.com>
2023-10-05 14:19:43 +0900

    ずんだもんじゃないのだ

--- omikuji/main.js
+++ omikuji/main.js
@@ -5,7 +5,7 @@
     const result = document.getElementById('result');
 
     btn.addEventListener('click', () => {
-        const results = ['大吉なのだ','中吉なのだ','凶なのだ','小吉なのだ','末吉なのだ','吉なのだ','大凶なのだ'];
+        const results = ['大吉なのだ','中吉なのだ','凶なのだ','小吉なのだ','末吉なのだ','吉なのだ','大凶なのだ','ずんだもんじゃないのだ'];
         const n = Math.floor(Math.random() * results.length);
 
         result.textContent = results[n];
